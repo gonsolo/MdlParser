199f142a5f226b2550da6841b873c71d7194885b
Gon Solo <gonsolo@gmail.com>
2021-04-17 10:20:07 +0200

    Read file and feed to parser.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CITRON		= ../../src/citron
 CITRON_SOURCE	= $(CITRON)/src
 
 run: mdl
-	./mdl
+	./mdl gun_metal.mdl
 mdl: $(CITRON_SOURCE)/CitronParser.swift $(CITRON_SOURCE)/CitronLexer.swift mdl.swift main.swift
 	swiftc $^ -o $@
 mdl.swift: mdl.y
--- main.swift
+++ main.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 enum Expression {
         case number(Int)
 }
@@ -9,3 +11,20 @@ typealias TokenData = (token: MdlParser.CitronToken, code: MdlParser.CitronToken
 let lexer = CitronLexer<TokenData>(rules: [
 
         ])
+
+guard CommandLine.argc == 2 else {
+        print("Usage: mdl <file>")
+        exit(0)
+}
+
+let fileName = CommandLine.arguments[1]
+let input = try String(contentsOfFile: fileName)
+do {
+        try lexer.tokenize(input) { (t, c) in 
+                try parser.consume(token: t, code: c)
+        }
+        let tree = try parser.endParsing()
+        print(tree)
+} catch (let error) {
+        print("Error \(error)")
+}
